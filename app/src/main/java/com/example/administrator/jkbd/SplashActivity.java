f50935198a9d4dece9b639104cf90fab1ba32834
lipangithub <271348929@qq.com>
2017-06-28 20:51:14 +0800

    闪屏后自动跳转到主页

--- app/src/main/java/com/example/administrator/jkbd/SplashActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.administrator.jkbd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,8 +12,24 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        mCountDownTimer.start();
+
     }
+    CountDownTimer mCountDownTimer=new CountDownTimer(2000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+
+        }
+    };
+
 }
