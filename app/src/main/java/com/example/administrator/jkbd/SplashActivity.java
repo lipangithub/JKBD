06ee554d9d62b142f2bd5d87e783f78ae3c6485c
lipangithub <271348929@qq.com>
2017-06-28 20:18:44 +0800

    添加图片和闪屏背景图

---
+++ app/src/main/java/com/example/administrator/jkbd/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.jkbd;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 李攀 on 2017/6/28.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
