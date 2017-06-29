054633518f376c6e1ef6c298a722988f84e8284d
lipangithub <271348929@qq.com>
2017-06-29 10:24:36 +0800

    把activity类放到对应包里

---
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.jkbd.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.administrator.jkbd.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+    }
+}
