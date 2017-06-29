dbb97cfe02c6c03925f2683728b64811135a02da
lipangithub <271348929@qq.com>
2017-06-29 21:58:05 +0800

    从网上获取考试信息

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.administrator.jkbd.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.administrator.jkbd.R;
+import com.example.administrator.jkbd.bean.Question;
+import com.example.administrator.jkbd.bean.item;
+import com.example.administrator.jkbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        OkHttpUtils<item> utils=new OkHttpUtils<>(getApplicationContext());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(uri)
+                .targetClass(item.class)
+                .execute(new OkHttpUtils.OnCompleteListener<item>(){
+
+                    @Override
+                    public void onSuccess(item result) {
+                        Log.e("main","result="+result);
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+
+                    }
+                });
         startActivity(new Intent(MainActivity.this,ExamActivity.class));
     }
 
