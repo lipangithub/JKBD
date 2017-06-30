5aa60ebfcb640e00e6e41abe5fa0e71a9314603a
lipangithub <271348929@qq.com>
2017-06-30 16:47:44 +0800

    添加ResultUtils类和随机显示考试题目

--- app/src/main/java/com/example/administrator/jkbd/ExamApplication.java
+++ app/src/main/java/com/example/administrator/jkbd/ExamApplication.java
@@ -4,10 +4,14 @@ import android.app.Application;
 import android.util.Log;
 
 import com.example.administrator.jkbd.bean.Question;
+import com.example.administrator.jkbd.bean.all;
 import com.example.administrator.jkbd.bean.item;
 import com.example.administrator.jkbd.utils.OkHttpUtils;
+import com.example.administrator.jkbd.utils.ResultUtils;
 
 import java.util.List;
+
+import static com.example.administrator.jkbd.utils.ResultUtils.getListResultFromJson;
 
 /**
  * Created by 李攀 on 2017/6/30.
@@ -29,25 +33,53 @@ public class ExamApplication extends Application {
     }
 
     private void initData() {
-        OkHttpUtils<item> utils=new OkHttpUtils<>(instance);
-        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
-        utils.url(uri)
-                .targetClass(item.class)
-                .execute(new OkHttpUtils.OnCompleteListener<item>(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpUtils<item> utils=new OkHttpUtils<>(instance);
+                String uri="http://101.251.196.90:8080/JztkServer/examInfo";
+                utils.url(uri)
+                        .targetClass(item.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<item>(){
 
-                    @Override
-                    public void onSuccess(item result) {
-                        Log.e("main","result="+result);
-                        mitem=result;
+                            @Override
+                            public void onSuccess(item result) {
+                                Log.e("main","result="+result);
+                                mitem=result;
 
-                    }
+                            }
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
+                OkHttpUtils<String> utils1=new OkHttpUtils<>(instance);
+                String url2="http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand";
+                utils1.url(url2)
+                        .targetClass(String.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<String>(){
 
-                    @Override
-                    public void onError(String error) {
-                        Log.e("main","error="+error);
+                            @Override
+                            public void onSuccess(String jsonStr) {
+                                all result = ResultUtils.getListResultFromJson(jsonStr);
+                                if(result!=null && result.getError_code()==0){
+                                    List<Question> list=result.getQuestions();
+                                    if (list!=null && list.size()>0){
+                                        mExamList=list;
+                                    }
+                                }
+                            }
 
-                    }
-                });
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+
+                            }
+                        });
+            }
+        }).start();
+
+
     }
 
     public item getMitem() {
--- app/src/main/java/com/example/administrator/jkbd/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/ExamActivity.java
@@ -4,18 +4,25 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.jkbd.ExamApplication;
 import com.example.administrator.jkbd.R;
+import com.example.administrator.jkbd.bean.Question;
 import com.example.administrator.jkbd.bean.item;
+
+import java.util.List;
+
+import static android.media.CamcorderProfile.get;
 
 /**
  * Created by 李攀 on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
-    TextView tvExamInfo;
+    TextView tvExamInfo,tvExamTitle,tv0p1,tv0p2,tv0p3,tv0p4;
+    ImageView mImageView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +33,33 @@ public class ExamActivity extends AppCompatActivity {
 
     private void initView() {
         tvExamInfo = (TextView) findViewById(R.id.tv_examinfo);
+        tvExamTitle = (TextView) findViewById(R.id.tv_exam_title);
+        tv0p1 = (TextView) findViewById(R.id.tv_op1);
+        tv0p2 = (TextView) findViewById(R.id.tv_op2);
+        tv0p3 = (TextView) findViewById(R.id.tv_op3);
+        tv0p4 = (TextView) findViewById(R.id.tv_op4);
     }
 
     private void initData() {
        item examInfo =ExamApplication.getInstance().getMitem();
         if (examInfo!=null){
             showData(examInfo);
+        }
+      List<Question> questionList= ExamApplication.getInstance().getmExamList();
+        if (questionList!=null ){
+            showExam(questionList);
+        }
+    }
+
+    private void showExam(List<Question> questionList) {
+        Question exam = questionList.get(0);
+        if (exam!=null){
+            tvExamTitle.setText(exam.getQuestion());
+            tv0p1.setText(exam.getItem1());
+            tv0p2.setText(exam.getItem2());
+            tv0p3.setText(exam.getItem3());
+            tv0p4.setText(exam.getItem4());
+
         }
     }
 
